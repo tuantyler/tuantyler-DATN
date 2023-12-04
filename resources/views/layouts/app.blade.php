02f6b88276f3d982a966e0e4accb6d18d9fe037f
tuantyler <tuantyler21@gmail.com>
2023-12-04 16:57:08 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@hasSection('template_title')@yield('template_title') | @endif {{ config('app.name', Lang::get('titles.app')) }}</title>
+        <meta name="description" content="">
+        <meta name="author" content="Jeremy Kenedy">
+        <link rel="shortcut icon" href="/favicon.ico">
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        {{-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --}}
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        @yield('template_linked_fonts')
+        @vite(['resources/assets/sass/app.scss', 'resources/assets/js/app.js'])
+        @yield('template_linked_css')
+        <style type="text/css">
+            @yield('template_fastload_css')
+            @if (Auth::User() && (Auth::User()->profile) && (Auth::User()->profile->avatar_status == 0))
+                .user-avatar-nav {
+                    background: url({{ Gravatar::get(Auth::user()->email) }}) 50% 50% no-repeat;
+                    background-size: auto 100%;
+                }
+            @endif
+        </style>        {{-- Scripts --}}
+        <script>
+            window.Laravel = {!! json_encode([
+                'csrfToken' => csrf_token(),
+            ]) !!};
+        </script>
+        @if (Auth::User() && (Auth::User()->profile) && $theme->link != null && $theme->link != 'null')
+            <link rel="stylesheet" type="text/css" href="{{ $theme->link }}">
+        @endif
+        @yield('head')
+        @include('scripts.ga-analytics')
+    </head>
+    <body>
+        <div id="app">
+            @include('partials.nav')
+            <main class="py-4">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            @include('partials.form-status')
+                        </div>
+                    </div>
+                </div>
+                @yield('content')
+            </main>
+        </div>
+        @if(config('settings.googleMapsAPIStatus'))
+            {!! HTML::script('//maps.googleapis.com/maps/api/js?key='.config("settings.googleMapsAPIKey").'&libraries=places&dummy=.js', array('type' => 'text/javascript')) !!}
+        @endif
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        @yield('footer_scripts')
+    </body>
+</html>
